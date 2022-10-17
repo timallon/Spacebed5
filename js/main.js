3b3923ce3f78dcb644bf951c48a21df928982c7e
Tim Allon <timallon@gmail.com>
2022-10-17 17:00:30 +0100

    asteroids are falling

--- js/main.js
+++ js/main.js
@@ -9,37 +9,61 @@ const bedY = canvasHeight * 0.85
 let moveRight;
 let moveLeft;
 let gameId = 0;
-
-
-//add song
-const song = new Audio("audio/deedee.mp3");
-song.volume = 0.5;
-
-//variables
 const background = new Image();
 background.src = "images/background.png";
 const bed = new Image();
 bed.src = "images/bed.png";
 const dylanSmile = new Image();
 dylanSmile.src = "images/dylan1.png";
+const meteor1 = new Image();
+meteor1.src = "images/meteorGrey_big1.png";
+let meteor1Y = 200;
+
+const meteor1Arr = [
+    { x: 275, y: -200, img: meteor1 },
+    { x: 75, y: -800, img: meteor1 },
+    { x: 275, y: -1200, img: meteor1 },
+]
+
+//add song
+//const song = new Audio("audio/deedee.mp3");
+//song.volume = 0.5;
+
+//variables
+
 
 window.onload = () => {
-    ctx.drawImage(background, 0, 0, canvasWidth, canvasHeight);
-    document.getElementById('start-button').onclick = () => {
+//    ctx.drawImage(background, 0, 0, canvasWidth, canvasHeight);
+//    document.getElementById('start-button').onclick = () => {
       startGame();
-    }
+//    }
 
     function startGame() {
         startScreen.style.display = "none";
         ctx.drawImage(background, 0, 0, canvasWidth, canvasHeight);
         ctx.drawImage(bed, bedX, bedY, canvasWidth * 0.1, canvasHeight * 0.1);
         ctx.drawImage(dylanSmile, bedX + (bed.width/27), bedY, bed.width * 0.055, bed.height * 0.07)
+//        ctx.drawImage(meteor1, 275, 275, 75, 75);
         gameId = requestAnimationFrame(startGame);
         if (moveRight === true) {
             bedX += canvasWidth * 0.01;
           } else if (moveLeft === true) {
             bedX -= canvasWidth * 0.01;
           }
+        
+              //traffic Cars
+    //ctx.drawImage(trafficCar1, 275, trafficCar1_Y, 100, 150);
+    //move the traffic car
+    //trafficCar1_Y += 3;
+        for (let i = 0; i < meteor1Arr.length; i += 1) {
+            let current = meteor1Arr[i];
+            ctx.drawImage(current.img, current.x, current.y, 100, 100);
+            current.y += 3;
+            if (current.y > canvas.height) {
+            current.y = -300;
+            }
+        }
+
 //    song.play(); 
     }
     // 
